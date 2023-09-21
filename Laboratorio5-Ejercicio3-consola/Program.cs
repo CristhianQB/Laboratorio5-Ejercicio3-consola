ea51fbbe35b8f390df6ebcbe456ae1d469f40f39
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-09-21 17:23:21 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio5-Ejercicio3-consola/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio5_Ejercicio3_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nùmero de notas: ");
+            float nnotas = float.Parse(Console.ReadLine());
+
+            float sumar = 0f;
+            for (int i = 1; i <= nnotas; i++)
+            {
+                Console.Write("Ingrese la nota " +i +" :");
+                float notas = float.Parse(Console.ReadLine());
+                sumar = sumar + notas;
+            }
+            float promedio = sumar / nnotas; 
+            Console.WriteLine("El promedio es: "+promedio);
+            Console.ReadKey();
+        }
+    }
+}
